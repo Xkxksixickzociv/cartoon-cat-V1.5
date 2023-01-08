c0efa251e3687cd1b34dbf8b03ae97524df7068a
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-08 20:29:46 +0200

    Add files via upload

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,5 +1,5 @@
 function onCreate()
-makeLuaText('watermark', songName .. " | Port By - ¡SantiYea! -", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
+makeLuaText('watermark', songName .. " | Port By - ¡SantiYea! - and ported apk by NickYT FNF", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
     setTextSize('watermark', 20);
     addLuaText('watermark');
 end
